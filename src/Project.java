c1ec276bb305298790af39fdca1dd66eae12d72e
927 <glebkalinin927@gmail.com>
2020-12-13 22:07:29 +0300

    .

---
+++ src/Project.java
@@ -0,0 +1,42 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            String s = readFromFile("input.txt");
+            System.out.println(calculator(s));
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static double calculator(String s) throws Exception{
+        String[] s1 = s.split(" ");
+            if (!s1[0].matches("\\d+") || !s1[2].matches("\\d+")) throw new Exception("Error! Not number");
+            if (!s1[1].matches("[/*+-]")) throw new Exception("Operation Error!");
+            if (Integer.parseInt(s1[2]) == 0&&s1[1].equals("/")) throw new Exception("Error! Division by zero");
+            double a = Double.parseDouble(s1[0]);
+            double b = Double.parseDouble(s1[2]);
+            if (s1[1].equals("/")) return a / b;
+            if (s1[1].equals("*")) return a * b;
+            if (s1[1].equals("+")) return a + b;
+            if (s1[1].equals("-")) return a - b;
+            return 0;
+    }
+
+    public static String readFromFile(String file_name) throws IOException{
+        FileReader reader = new FileReader("input.txt");
+        String s="";
+        int c;
+        while ((c=reader.read())!=-1){
+            s+=(char)c;
+        }
+        return s;
+    }
+}
